dab0fd91e4e993fcaaff490878c414601c4e9644
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-09-21 21:24:16 +0100

    Updated solution - closer look at the code to identify the issues.

--- Breadcrumb_Generator/solution.py
+++ Breadcrumb_Generator/solution.py
@@ -1,63 +1,36 @@
 def generate_bc(url, separator):
-    # Define words to ignore while acronymizing
+    # Define words to ignore during acronymization
     ignore_words = ["the", "of", "in", "from", "by", "with", "and", "or", "for", "to", "at", "a"]
 
-    # Split the URL by '/' and remove empty elements
-    path_components = [component for component in url.split('/') if component]
+    # Parse the URL to extract components
+    components = url.split("/")
 
-    # Handle the root/home case
-    if not path_components:
-        return '<span class="active">HOME</span>'
+    # Remove empty and redundant elements
+    components = [c for c in components if c != "" and c != "index"]
 
-    # Initialize the breadcrumb with the HOME element
+    # Initialize the breadcrumb trail with the HOME element
     breadcrumb = ['<a href="/">HOME</a>']
 
-    # Initialize the path to the root
-    path = '/'
+    # Initialize the current path
+    current_path = '/'
 
-    # Process each path component
-    for component in path_components:
-        # Remove common extensions
-        if '.' in component:
-            component, ext = component.split('.', 1)
-            if ext in ("html", "htm", "php", "asp"):
-                component = component.rsplit('-', 1)[0]
-
-        # Handle "index.something" as if it's not there
-        if component.startswith("index."):
-            continue
-
-        # Remove anchors and parameters
-        if '#' in component:
-            component = component.split('#', 1)[0]
-        if '?' in component:
-            component = component.split('?', 1)[0]
-
-        # Acronymize long components
-        acronym = ""
-        words = component.split('-')
-        for word in words:
-            if word not in ignore_words:
-                acronym += word[0]
-        if len(acronym) > 0:
+    for component in components:
+        # Handle acronymization for long components
+        if len(component) > 30:
+            words = [word for word in component.split('-') if word not in ignore_words]
+            acronym = ''.join(word[0].upper() for word in words)
             component = acronym
 
-        # Shorten if longer than 30 characters
-        if len(component) > 30:
-            component = component[:30]
+        # Update the current path
+        current_path += component + '/'
 
-        # Build the path
-        path += component + '/'
-
-        # Create the breadcrumb element
-        breadcrumb.append(f'<a href="{path}">{component.upper()}</a>')
-
-    # Set the last element as active
-    breadcrumb[-1] = f'<span class="active">{breadcrumb[-1]}</span>'
+        # Check if it's the last element
+        if component.endswith(('.html', '.htm', '.php', '.asp')):
+            breadcrumb.append('<span class="active">' + component[:-5].upper() + '</span>')
+        else:
+            breadcrumb.append('<a href="' + current_path + '">' + component.upper() + '</a>')
 
     # Join the breadcrumb elements with the separator
-    return separator.join(breadcrumb)
+    result = separator.join(breadcrumb)
 
-# Test cases
-print(generate_bc("mysite.com/pictures/holidays.html", " : "))
-print(generate_bc("www.codewars.com/users/GiacomoSorbi", " / "))
+    return result
